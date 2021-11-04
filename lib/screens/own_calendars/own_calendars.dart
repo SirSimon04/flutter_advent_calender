a1cf73c75d0a77fe9ee46418a19c41b04c83f872
SirSimon04 <simi@engelnetz.de>
2021-11-04 20:37:30 +0100

    build own_calendars.dart page

--- lib/screens/own_calendars/own_calendars.dart
+++ lib/screens/own_calendars/own_calendars.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advent_calender/screens/calendar_view/calendar_view.dart';
 
 class OwnCalendars extends StatefulWidget {
   const OwnCalendars({Key? key}) : super(key: key);
@@ -12,7 +14,180 @@ class _OwnCalendarsState extends State<OwnCalendars>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Meine Kalender",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(CupertinoPageRoute(
+                    builder: (context) => const CalendarView()));
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Christina",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Du kannst ein neues Türchen öffnen",
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 15,
+                    right: 30,
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(
+                        "assets/present.png",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(CupertinoPageRoute(
+                    builder: (context) => const CalendarView()));
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Christina",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Du hast bisher alle Türchen geöffnet",
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 15,
+                    right: 30,
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(
+                        "assets/present.png",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(CupertinoPageRoute(
+                    builder: (context) => const CalendarView()));
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Christina",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Du kannst zwei neue Türchen öffnen",
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 15,
+                    right: 30,
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(
+                        "assets/present.png",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
