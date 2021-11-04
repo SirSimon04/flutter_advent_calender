fe2d9553fd086e6804109fb976cd626d24e1a00c
SirSimon04 <simi@engelnetz.de>
2021-11-04 16:24:39 +0100

    added BottomNavigationBar to change between pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advent_calender/screens/create_calendar/create_calendar.dart';
+import 'package:flutter_advent_calender/screens/own_calendars/own_calendars.dart';
 import 'package:flutter_advent_calender/widgets/calendar_door.dart';
 import 'package:flutter_advent_calender/widgets/plain_door.dart';
 
@@ -14,7 +16,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: MyApp(),
+      home: const MyApp(),
     );
   }
 }
@@ -27,8 +29,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = [
+    OwnCalendars(),
+    CreateCalendar(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_view_day),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_rounded),
+          ),
+        ],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
