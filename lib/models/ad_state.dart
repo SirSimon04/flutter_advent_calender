b708bc7e2b87c2e45c6c9bf5d85e59808e79af9d
SirSimon04 <simi@engelnetz.de>
2021-11-21 21:41:42 +0100

    updated ad ids

--- lib/models/ad_state.dart
+++ lib/models/ad_state.dart
@@ -7,10 +7,10 @@ class AdState {
   AdState(this.initialization);
 
   String get calendarViewBottomAd => Platform.isAndroid
-      ? "ca-app-pub-3940256099942544/6300978111"
+      ? "ca-app-pub-1042536658048489/4283857849"
       : "ca-app-pub-3940256099942544/2934735716";
 
   String get calendarsViewBottomAd => Platform.isAndroid
-      ? "ca-app-pub-3940256099942544/6300978111"
+      ? "ca-app-pub-1042536658048489/6982366893"
       : "ca-app-pub-3940256099942544/2934735716";
 }
